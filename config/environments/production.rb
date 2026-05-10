7d09497490d7fd9017be626e79f2e3ada0cae74f
CM Analytics Dev <dev@cmanalytics.pt>
2026-05-10 14:40:55 -0300

    Fix production.rb - remove asset config for Rails 8 compatibility

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,6 @@ Rails.application.configure do
   config.assets.compress = true
   config.log_level = :info
   config.log_tags = [ :request_id ]
-  config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
   config.active_support.report_deprecations = false
   config.log_formatter = ::Logger::Formatter.new
@@ -25,12 +24,4 @@ Rails.application.configure do
 
   # Active Job
   config.active_job.queue_adapter = :sidekiq
-
-  # CORS allowed origins
-  config.allowed_cors_origins = [
-    "https://www.correiodamanha.com.br",
-    "https://correiodamanha.com.br",
-    "https://cm-analytics.onrender.com",
-    "http://localhost:3000"
-  ]
 end
