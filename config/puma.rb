0f8f93877eee830dcf2e13ac9b14e6e7407e58a6
CM Analytics Dev <dev@cmanalytics.pt>
2026-05-10 14:35:07 -0300

    Fix Puma config - use single worker mode for Render

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,11 @@
-# Puma configuration file
+# Puma configuration file for production
 
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
+
+# Don't use cluster mode on Render (single worker)
+workers ENV.fetch("WEB_CONCURRENCY") { 0 }
 
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
@@ -14,5 +17,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 plugin :tmp_restart
 
-# Allow puma to be restarted by `rails restart` command
-plugin :tmp_restart
+preload_app!
